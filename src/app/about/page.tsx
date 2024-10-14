1b3110882737266e9f7ec811b0478e17a9c7c615
Jean Carlos Nesi <jean.carlos_nesi@hotmail.com>
2024-10-14 19:21:08 -0300

    feat: update page 'about'

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -38,13 +38,16 @@ export default function AboutPage() {
           <div className="flex flex-col gap-6">
             <h2 className="text-4xl font-bold">Sobre mim</h2>
             <p className="text-gray-700 dark:text-gray-300">
-              Meu nome é Jean, e tenho {calcAge(new Date(2004, 0, 9))} anos.
+              Olá, sou Jean, tenho {calcAge(new Date(2004, 0, 9))} anos e estou
+              cursando Engenharia de Software.
               <br />
               <br />
-              Como desenvolvedor front-end, tenho aproximadamente{" "}
+              Como desenvolvedor front-end, venho acumulando cerca de{" "}
               {calcAge(new Date(2022, 3, 18))} anos de experiência trabalhando
               com tecnologias como ReactJS, TypeScript, Styled-components, HTML,
-              CSS, Next.js, Git,{" "}
+              CSS, Next.js, Git, entre outras. Sou apaixonado por criar
+              interfaces dinâmicas e funcionais que proporcionam uma experiência
+              incrível ao usuário.
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger>
@@ -61,13 +64,12 @@ export default function AboutPage() {
               oferecem uma excelente experiência ao usuário.
               <br />
               <br />
-              Com uma sólida base em tecnologias como React, TypeScript, e
-              Node.js, desenvolvi soluções eficazes e inovadoras ao longo da
-              minha carreira como desenvolvedor front-end. Além disso, possuo
-              ampla experiência em metodologias ágeis como Scrum e Kanban, o que
-              me permite colaborar de forma eficiente e dinâmica com equipes de
-              desenvolvimento. Estou à disposição para contribuir com minha
-              expertise e ajudar a impulsionar seus projetos.
+              Com uma base sólida em React, TypeScript e Node.js, já tive a
+              oportunidade de desenvolver soluções eficazes e inovadoras em
+              projetos que contribuíram para minha evolução profissional. Além
+              disso, tenho experiência prática com metodologias ágeis como Scrum
+              e Kanban, o que me permite colaborar de forma eficiente em equipes
+              de desenvolvimento, garantindo sempre um trabalho bem estruturado.
             </p>
           </div>
         </div>
